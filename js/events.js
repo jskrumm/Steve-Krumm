f9d2a14fc996af9df5083c8e665ec1fde4dfb404
Joshua Krumm <jskrumm@gmail.com>
2012-06-04 20:31:48 -0400

    Modulized CSS and fixed form errors.

---
+++ js/events.js
@@ -0,0 +1,22 @@
+(function ($) {
+    /*$("addEvent").submit(function(){
+        $.post(
+            "form.cshtml",
+            $(this).serialize(), 
+            function(data){
+                
+            }
+        );
+        return false;
+    });*/
+    
+    $("#addEvent").validate({
+        submitHandler: function(form) {
+            form.submit();
+        },
+        errorContainer: $("#formErrors"),
+        errorLabelContainer: $("ol",  $("#formErrors")),
+        wrapper: 'li',
+        meta: "validate" 
+    });
+ })(jQuery);
