32d740cc5c2c369fcceb309935fada0a25dde09a
Joshua Krumm <jskrumm@gmail.com>
2012-11-17 16:19:54 -0500

    Updated forms

--- js/events.js
+++ js/events.js
@@ -1,34 +1,37 @@
 (function ($) {
-    /*$("addEvent").submit(function(){
-    $.post(
-    "form.cshtml",
-    $(this).serialize(), 
-    function(data){
-                
-    }
-    );
-    return false;
-    });*/
-
-    $("#addEvent").validate({
-        submitHandler: function (form) {
-            form.submit();
-        },
-        errorContainer: $("#formErrors"),
-        errorLabelContainer: $("ol", $("#formErrors")),
-        wrapper: 'li',
-        meta: "validate"
-    });
 
     $.getJSON('/GetEventJSON/', function (data) {
-        console.log(data);
-        console.log(data[3]);
-        console.log(data.StartDate);
-        console.log(formatJSONDate(Date(data.Start_Date)));
+        $.each(data, function (key, val) {
+            //console.log(formatJSONDate(val["Start Date"]));
+            //console.log(formatJSONDate(val["End Date"]));
+        });
     });
 
     function formatJSONDate(jsonDate) {
         var date = new Date(parseInt(jsonDate.substr(6)));
         return date;
     }
+
+    $('#addEvent').submit(function (e) {
+        e.preventDefault();
+        $(this).validate({
+            submitHandler: function (form) {
+                console.log("hit");
+                console.log($("#addEvent").serialize());
+                $.ajax({
+                    type: "POST",
+                    url: "/POSTEventFormData/",
+                    data: $("#addEvent").serialize(),
+                    dataType: "text/plain",
+                    success: function (response) {
+                        console.log(response);
+                    }
+                });
+            },
+            errorContainer: $("#formErrors"),
+            errorLabelContainer: $("ol", $("#formErrors")),
+            wrapper: 'li',
+            meta: "validate"
+        });
+    });
 })(jQuery);
