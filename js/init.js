7f11b28d9f9384c8ec4c5f4928f1c716d836bfbd
Josh Krumm <jskrumm@gmail.com>
2013-02-16 15:00:53 -0500

    Worked on mobile version

--- js/init.js
+++ js/init.js
@@ -1,3 +1,9 @@
-$( document ).ready( function () {
+(function ($) {
+    $(".mobile-nav .menu-button").on("click", function () {
+        var $nav = $(this).next();
 
-});
+        $nav.animate({"right" :  parseInt($nav.css('right'),10) >= 0 ? "-=200px" : "+=200px"}, "slow");
+
+       
+    });
+})(jQuery);
